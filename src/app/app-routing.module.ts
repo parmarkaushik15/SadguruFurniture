227317a328faaa958f41b02981f12670bc393b04
kaushik parmar <kaushik parmar>
2018-03-09 13:16:48 +0530

    Default Style Theme apply

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'admin', children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
