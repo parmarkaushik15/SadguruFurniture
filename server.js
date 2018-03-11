0b7fc3b75c36bb0fa33ad06c5f19624c9de6c92a
kaushik parmar <kaushik parmar>
2018-03-11 22:06:39 +0530

    Node server route setting

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
+app.get('/*', function(req, res){
+	res.sendFile('dist/index.html', { root: __dirname });
+});
+app.get('/app/*', function(req, res){
+	res.sendFile('dist/index.html', { root: __dirname });
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
